846b1ff5f40e55ea53241e0eecca467b8d926d72
Marc Vouve <mvouve@gmail.com>
2017-05-25 16:58:48 -0700

    updated installcustomprograms to actually run without getting hung up

--- installcustomprograms.sh
+++ installcustomprograms.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 #Download server files
-wget marcvouve.com/files/VPNBuddyServer.tar.gz
+wget marcvouve.com/files/filter-api.tar.gz
 #untar Server files (
-tar -zxvf VPNBuddyServer.tar.gz
+tar -zxvf filter-api.tar.gz
 apt install -y libpqxx-dev postgresql daemon
 sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'postgres'"
-nohup ./vpnbuddyapi
 ./VPNBuddyFilter
+nohup ./vpnbuddyapi
+
